ceda4d33dab853e3c9ed08411e81ef0aa98357bb
Arne Babenhauserheide <arne.babenhauserheide@disy.net>
2019-11-07 18:25:54 +0100

    add global results

--- test-interaction.sh
+++ test-interaction.sh
@@ -58,8 +58,8 @@ echo "jenkins checks changes: cde is bad"
 echo "for this prototype let’s just assume that the commit failed to build"
 (cd jenkins; for i in $(tac unprocessed_commits.log); do hg prune -r $i && (echo "to dev2: commit $i was contained in a failing build. It has been pruned. Please graft and fix it and push again." && echo $i >> ../dev2/failing_commits.log); hg evolve; done; hg log --graph)
 
-echo "dev1 pushes, has to rebase"
-(cd dev1; hg pull; hg log --graph; hg push || hg pull --rebase ; hg log --graph)
+echo "dev1 pushes, the push succeeds, because the local hg does not know about the pruning"
+(cd dev1; hg push ; hg log --graph)
 
 echo "dev2 does some more work and tries to push, but since the bad commits leading to the good change from dev1 were removed, this would push orphan changesets, so the push fails."
 (cd dev2; echo efg > foo; hg ci -A -m efg; hg pull --rebase; hg push; hg log --graph)
@@ -68,7 +68,7 @@ echo "dev2 evolves the repo and only has the good changes left."
 (cd dev2; hg evolve --any; hg push; hg log --graph)
 
 echo "now dev2 receives the mails to graph the commits."
-# TODO: bug in mercurial: graft does not respect HGMERGE
+# TODO: bug: graft does not respect HGMERGE
 (cd dev2; for i in $(tac failing_commits.log); do hg graft --tool internal:merge-local --hidden $i; done; hg log --graph)
 
 echo "dev2 fixes the commit and pushes"
@@ -77,4 +77,13 @@ echo "dev2 fixes the commit and pushes"
 echo "jenkins checks the new changes and accepts them."
 (cd jenkins; hg phase --public -r tip; rm unprocessed_commits.log; hg log --graph)
 
-echo "result: the repository is as if dev2 had pushed after dev1-x and had fixed the change before pushing"
+echo "local result: the jenkins repository is as if dev2 had pushed after dev1-x and had fixed the change before pushing"
+
+echo "now dev1 pulls and gets the data about obsoletes"
+# TODO: bug: hg pull -u does not update to the new tip
+(cd dev1; hg pull; hg log --graph)
+echo "dev1 can fix this automatically with evolve, or have evolve do it automatically as part of rebase"
+# (cd dev1; hg evolve --any; hg log --graph)
+(cd dev1; hg rollback; hg pull --rebase; hg log --graph)
+
+echo "global result: now all developers see a history in which dev2 committed after dev1 and fixed the bad commit before pushing"
